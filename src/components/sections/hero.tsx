2f275de2f1718336b48aa217b941c10dc7e47ce1
luca ion <lucaion781@gmail.com>
2025-12-30 23:21:08 +0000

    added animations on hero and finished about me page

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -3,50 +3,45 @@
 import { profile } from '@/lib/data';
 import Socials from '@/components/shared/socials';
 import { FadeIn, FadeInStagger } from '@/components/shared/fade-in';
-import { TypeAnimation } from 'react-type-animation';
-import Image from 'next/image';
+import dynamic from 'next/dynamic';
+
+const TypeAnimation = dynamic(
+  () => import('react-type-animation').then((mod) => mod.TypeAnimation),
+  { ssr: false }
+);
 
 export default function Hero() {
   return (
     <section
       id="home"
-      className="w-full min-h-[calc(100vh-4rem)] flex items-center justify-center bg-background"
+      className="w-full h-[calc(100vh-4rem)] flex items-center justify-center bg-background"
     >
-      <div className="container grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
-        <div className="space-y-4">
-          <FadeInStagger>
-            <FadeIn>
-              <TypeAnimation
-                sequence={[
-                  profile.name,
-                  1000,
-                ]}
-                wrapper="h1"
-                speed={10}
-                className="font-headline text-4xl font-bold tracking-tighter text-primary sm:text-5xl md:text-6xl lg:text-7xl"
-              />
-            </FadeIn>
-            <FadeIn>
-              <p className="max-w-2xl mt-4 text-lg text-muted-foreground md:text-xl">
-                {profile.position}
-              </p>
-            </FadeIn>
-            <FadeIn>
-              <div className="mt-6">
-                <Socials />
-              </div>
-            </FadeIn>
-          </FadeInStagger>
-        </div>
-        <div className="flex justify-center items-center">
-           <Image
-            src="/profile.jpeg"
-            alt="Luca Petru Ion"
-            width={300}
-            height={300}
-            className="rounded-full object-cover w-[300px] h-[300px] border-4 border-primary/20 shadow-lg"
+      <div className="container">
+        <FadeInStagger>
+          <FadeIn>
+          <TypeAnimation
+            sequence={[profile.name, 900]}
+            wrapper="h1"
+            speed={10}
+            cursor={false}
+            className="
+              hero-typed
+              font-headline text-4xl font-bold tracking-tighter text-primary
+              sm:text-5xl md:text-6xl lg:text-7xl
+            "
           />
-        </div>
+          </FadeIn>
+          <FadeIn>
+            <p className="max-w-2xl mt-4 text-lg text-muted-foreground md:text-xl">
+              {profile.position}
+            </p>
+          </FadeIn>
+          <FadeIn>
+            <div className="mt-6">
+              <Socials />
+            </div>
+          </FadeIn>
+        </FadeInStagger>
       </div>
     </section>
   );
